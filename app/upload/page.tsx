6939768b6aad0755849d9a423d2b9be7940261c2
Akash Singh <akashsingh2210670@gmail.com>
2024-05-12 11:33:46 +0530

    Upload UI Improvements

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,12 +1,11 @@
-"use client"
-
+'use client'
 import React, { useState } from 'react';
 import { useEdgeStore } from "../lib/edgestore";
 import { set, update } from 'firebase/database';
 import { doc, getDocs, setDoc , collection, updateDoc } from 'firebase/firestore';
 import {auth ,  db } from '../firebase';
 import { ToastContainer, toast } from 'react-toastify';
-  import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css';
 import { useSearchParams , useRouter } from 'next/navigation';
 const UploadPage = () => {
   const search  = useSearchParams();
@@ -26,6 +25,7 @@ const UploadPage = () => {
     bank : [],
     // Add more document types as needed
   });
+  const [isUploading, setIsUploading] = useState(false);
 
   const handleFileChange = (e: { target: { files: any; }; }, type: any) => {
     const selectedFiles = e.target.files;
@@ -36,29 +36,27 @@ const UploadPage = () => {
   };
 
   const handleUpload = async () => {
-    console.log(downloadLinks);
-    //add these download links to firebase
-    try{
-      if(userId){
-    const applicationRef = getDocs(collection(db, "applications"));
-    (await applicationRef).forEach(async (doc) => {
-      console.log(doc.data().userId);
-      if(doc.data().userId === userId ){
-        await updateDoc(doc.ref, {
-          documents: downloadLinks,
+    if (window.confirm("Are you sure you want to upload the files?")) {
+      setIsUploading(true);
+      try {
+        if (userId) {
+          const applicationRef = getDocs(collection(db, "applications"));
+          (await applicationRef).forEach(async (doc) => {
+            if (doc.data().userId === userId) {
+              await updateDoc(doc.ref, {
+                documents: downloadLinks,
+              });
+            }
+          });
+        }
+        toast.success("Files uploaded successfully!");
+        router.push("/pitch/?id=" + applicationNumber + "&userId=" + userId);
+      } catch (e) {
+        console.error(e);
+        toast.error("Error uploading files!");
       }
-    )
-    }}
-    );
+      setIsUploading(false);
     }
-    toast.success("Files uploaded successfully!");
-    router.push("/pitch/?id="+applicationNumber+"&userId="+userId);
-  }
-    catch(e){
-      console.error(e);
-      toast.error("Error uploading files!");
-    }
-    console.log("Files uploaded successfully!");
   };
 
   return (
@@ -73,119 +71,119 @@ const UploadPage = () => {
           onChange={(e) => setAdhaarFile(e.target.files ? e.target.files[0] : undefined)}
         />
         <button
-        onClick={async () => {
-          if (adhaarFile) {
-            const res = await edgestore.publicFiles.upload({
-              file: adhaarFile,
-              onProgressChange: (progress) => {
-                // you can use this to show a progress bar
-                console.log(progress);
-              },
-            });
-            // you can run some server action or api here
-            // to add the necessary data to your database
-            console.log(res);
-            if (res.url) {
-              setDownloadLinks((prevLinks: any) => [...prevLinks, res.url]);
+          onClick={async () => {
+            if (adhaarFile && window.confirm("Are you sure you want to upload this file?")) {
+              const res = await edgestore.publicFiles.upload({
+                file: adhaarFile,
+                onProgressChange: (progress) => {
+                  console.log(progress);
+                },
+              });
+              console.log(res);
+              if (res.url) {
+                setDownloadLinks((prevLinks: any) => [...prevLinks, res.url]);
+              }
             }
-          }
-        }}
-      >
-        Upload
-      </button>
+          }}
+          disabled={isUploading}
+          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+        >
+          Upload
+        </button>
       </div>
       <div className="mb-4">
-        <label className="block mb-2">Income Proof (ITR):</label>
+        <label className="block mb-2">Income Tax Returns:</label>
         <input
           type="file"
           className="border rounded p-2 w-full"
-          
           onChange={(e) => setItrFile(e.target.files ? e.target.files[0] : undefined)}
         />
-        <button 
-        onClick={async () => {
-          if (itrFile) {
-            const res = await edgestore.publicFiles.upload({
-              file:itrFile,
-              onProgressChange: (progress) => {
-                // you can use this to show a progress bar
-                console.log(progress);
-              },
-            });
-            // you can run some server action or api here
-            // to add the necessary data to your database
-            console.log(res);
-            setDownloadLinks((prevLinks: any) => [...prevLinks, res.url]);
-          }
-        }}
-        >Upload</button>
-
+        <button
+          onClick={async () => {
+            if (itrFile && window.confirm("Are you sure you want to upload this file?")) {
+              const res = await edgestore.publicFiles.upload({
+                file: itrFile,
+                onProgressChange: (progress) => {
+                  console.log(progress);
+                },
+              });
+              console.log(res);
+              if (res.url) {
+                setDownloadLinks((prevLinks: any) => [...prevLinks, res.url]);
+              }
+            }
+          }}
+          disabled={isUploading}
+          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+        >
+          Upload
+        </button>
       </div>
       <div className="mb-4">
         <label className="block mb-2">Address Proof:</label>
         <input
           type="file"
           className="border rounded p-2 w-full"
-          
           onChange={(e) => setAddressFile(e.target.files ? e.target.files[0] : undefined)}
         />
         <button
-        onClick={async () => {
-          if (addressFile) {
-            const res = await edgestore.publicFiles.upload({
-              file: addressFile,
-              onProgressChange: (progress) => {
-                // you can use this to show a progress bar
-                console.log(progress);
-              },
-            });
-            // you can run some server action or api here
-            // to add the necessary data to your database
-            console.log(res);
-            setDownloadLinks((prevLinks: any) => [...prevLinks, res.url]);
-          }
-        }}
-        >Upload</button>
-      </div>
-
-      <div className="mb-4">
-        <label className="block mb-2">Bank Statement:</label>
-        <input
-          type="file"
-          className="border rounded p-2 w-full"
-          
-          onChange={(e) => setBankFile(e.target.files ? e.target.files[0] : undefined)}
-
-        />
-  <button
-        onClick={async () => {
-          if (bankFile) {
-            const res = await edgestore.publicFiles.upload({
-              file: bankFile,
-              onProgressChange: (progress) => {
-                // you can use this to show a progress bar
-                console.log(progress);
-              },
-            });
-            // you can run some server action or api here
-            // to add the necessary data to your database
-            console.log(res);
-            setDownloadLinks((prevLinks: any) => [...prevLinks, res.url]);
-          }
-        }}
-        >Upload</button>
-      </div>
-
-      <div>
-        <button
-          onClick={handleUpload}
+          onClick={async () => {
+            if (addressFile && window.confirm("Are you sure you want to upload this file?")) {
+              const res = await edgestore.publicFiles.upload({
+                file: addressFile,
+                onProgressChange: (progress) => {
+                  console.log(progress);
+                },
+              });
+              console.log(res);
+              if (res.url) {
+                setDownloadLinks((prevLinks: any) => [...prevLinks, res.url]);
+              }
+            }
+          }}
+          disabled={isUploading}
           className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
         >
           Upload
         </button>
       </div>
-      {/* Display selected files for each document type */}
-      
+      <div className="mb-4">
+        <label className="block mb-2">Bank Statement:</label>
+        <input
+          type="file"
+          className="border rounded p-2 w-full"
+          onChange={(e) => setBankFile(e.target.files ? e.target.files[0] : undefined)}
+        />
+        <button
+          onClick={async () => {
+            if (bankFile && window.confirm("Are you sure you want to upload this file?")) {
+              const res = await edgestore.publicFiles.upload({
+                file: bankFile,
+                onProgressChange: (progress) => {
+                  console.log(progress);
+                },
+              });
+              console.log(res);
+              if (res.url) {
+                setDownloadLinks((prevLinks: any) => [...prevLinks, res.url]);
+              }
+            }
+          }}
+          disabled={isUploading}
+          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+        >
+          Upload
+        </button>
+      </div>
+      <div>
+        <button
+          onClick={handleUpload}
+          disabled={isUploading}
+          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+        >
+          Submit
+        </button>
+      </div>
     </div>
   );
 };
